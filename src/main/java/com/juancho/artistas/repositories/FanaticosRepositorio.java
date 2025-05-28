08632d5b9c1a966c32ee450e4194c7fd8dd28496
Juanchito1106 <jandrescorrea19@gmail.com>
2025-05-27 19:28:58 -0500

    Pequeñas modificaciones hechas en clase 27 de Mayo

---
+++ src/main/java/com/juancho/artistas/repositories/FanaticosRepositorio.java
@@ -0,0 +1,8 @@
+package com.juancho.artistas.repositories;
+
+import com.juancho.artistas.model.Fanaticos;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface FanaticosRepositorio  extends CrudRepository<Fanaticos, Integer> {
+}
